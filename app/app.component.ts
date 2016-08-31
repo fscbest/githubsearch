4fa205cd87f86f9e4167bb6edde68efd91f75b05
fscbest <romangeyber@gmail.com>
2016-08-31 16:50:02 +0300

    Add bootstrap and Navbar

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,8 +4,9 @@ import { HTTP_PROVIDERS } from '@angular/http';
 import {GithubService} from "./services/github.service";
 
 @Component({
+  moduleId: module.id,
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1><profile></profile>',
+    templateUrl: 'app.component.html',
   directives: [ProfileComponent],
   providers: [HTTP_PROVIDERS, GithubService]
 })
