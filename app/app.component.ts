b47d5715c250c8dde203647eeb12d7572eef38f6
fscbest <romangeyber@gmail.com>
2016-08-31 14:29:31 +0300

    Add GithubService @

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import {ProfileComponent} from "./components/profile.component";
+import { HTTP_PROVIDERS } from '@angular/http';
+import {GithubService} from "./services/github.service";
 
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App</h1><profile></profile>',
-  directives: [ProfileComponent]
+  directives: [ProfileComponent],
+  providers: [HTTP_PROVIDERS, GithubService]
 })
 export class AppComponent { }
